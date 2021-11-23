b8b19c0d93ac88f24f8690bbbabafd011e04bf46
ItWasntMe796 <94887036+ItWasntMe796@users.noreply.github.com>
2021-11-23 11:33:18 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
 
-//declare a class to sort and display data:
-class sort_data{
-  //access modifiers
-  public:
-  void sort(int a){}
-};
-
 int main(){
   //declaring nessacary variables
   int a[4], sum = 0, ave, tmp;
@@ -17,31 +10,30 @@ int main(){
   for (int i; i < 5; ++i) {
     std::cin >> a[i];
     //add user input
-    sum = sum + a[i];
   }
   
   //display user input
-  std::cout << "Your numbers: ";
+  std::cout << "\nYour numbers: \n";
   for (int i; i < 5; ++i) {
-    std::cout << a[i] << "\n";
+    std::cout << "  " << a[i] << "\n";
+    sum = sum + a[i];
   }
-  
-  //display the sum of user input
-  std::cout << "\n" << "Sum: " << sum;
   
   //sorting user input:
   for (int i; i < 5; ++i){
     tmp = a[i-1];
     if (tmp > a[i]){
-      a[i-1] = a[i]
+      a[i-1] = a[i];
       a[i] = tmp;
     }
   }
   
-  std::cout << "Ordered numbers";
+  std::cout << "\nOrdered numbers\n";
   
   //display ordered numbers:
   for (int i; i < 5; ++i){
-    std::cout << a[i] << "\n";
+    std::cout << "  " << a[i] << "\n";
   }
+  
+  return 0;
 }
